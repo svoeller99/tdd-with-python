571e79042f3601c0932a9cc1b0e9b4f64f5cacfb
Sean Voeller <sean.voeller@gmail.com>
2015-07-10 20:34:07 -0700

    stuff and things

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-  return HttpResponse('<html><title>To-Do lists</title></html>')
+  return HttpResponse("""<html>
+    <title>To-Do lists</title>
+    <body>
+      <h1>To-Do</h1>
+      <input id="id_new_item" placeholder="Enter a to-do item" type="text" />
+      <table id="id_list_table">
+        <tr>
+          <td>1: Buy peacock feathers</td>
+        </tr>
+      </table>
+    </body>
+  </html>""")
